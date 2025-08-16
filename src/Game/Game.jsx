4c0b32db2fac3db6e5da15796dfab729963b91d1
Hamida AbdelKarim <hamidakaim67@example.com>
2025-08-16 07:59:51 -0700

    build first function of HandleTic-Tac-Teo

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -1,29 +1,34 @@
 import { useState } from "react";
 import styles from "./Came.module.css";
-function Board({ isX, onClik }) {
+function Square({ value, onClick }) {
+    return (
+        <button onClick={onClick}>
+            <span>{value}</span>
+        </button>
+    )
+}
+function Board() {
+    const [isXTurn, SetisXTrun] = useState(true);
+    const [TicTacTeo, SetTicTacTeo] = useState(Array(9).fill(""));
+
+    function HandleTicTacTeo(value) {
+        const Squares = [...TicTacTeo];
+        Squares[value] =
+            isXTurn ? Squares[value] = "X" : Squares[value] = "O"
+        SetTicTacTeo(Squares);
+        SetisXTrun(!isXTurn);
+    }
     return (
         <div className={styles.buttons}>
-            <button onClick={onClik}><span>{isX ? "X" : "O"}</span></button>
-            <button onClick={onClik}><span>{isX ? "X" : "O"}</span></button>
-            <button onClick={onClik}><span>{isX ? "X" : "O"}</span></button>
-            <button onClick={onClik}><span>{isX ? "X" : "O"}</span></button>
-            <button onClick={onClik}><span>{isX ? "X" : "O"}</span></button>
-            <button onClick={onClik}><span>{isX ? "X" : "O"}</span></button>
-            <button onClick={onClik}><span>{isX ? "X" : "O"}</span></button>
-            <button onClick={onClik}><span>{isX ? "X" : "O"}</span></button>
-            <button onClick={onClik}><span>{isX ? "X" : "O"}</span></button>
+            {TicTacTeo.map((value, index) => (<Square value={value} onClick={() => HandleTicTacTeo(index)} />))}
         </div>
     )
 }
 export default function Game() {
-    const [isX, SetisX] = useState(true);
-    function HandelisX() {
-        SetisX(!isX);
-    }
     return (
         <div className={styles.container}>
             <h1>PLAYER X START</h1>
-            <Board isX={isX} onClik={HandelisX} />
+            <Board />
         </div>
     )
 }
